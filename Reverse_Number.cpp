786f40f6b52cb9929623886ce3103e6e7708aca0
RACHIT6 <89304686+RACHIT6@users.noreply.github.com>
2021-10-23 18:15:55 +0530

    Update Reverse_Number.cpp

--- Reverse_Number.cpp
+++ Reverse_Number.cpp
@@ -10,14 +10,10 @@ int main(int argc, char const *argv[])
 
     while (num != 0)
     {
-        /*rem=n%10;    
-     reverse=reverse*10+rem;    
-     n/=10; */
         n = num % 10;
         reverse = reverse * 10 + n;
         num /= 10;
     }
-
     
     cout<<"--------------------------------------------------------------------------"<<endl<<endl;
     cout << "The reversed number is " << reverse << endl;
